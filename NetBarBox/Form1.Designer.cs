17d041e174d4c42eae46f95486fd66937e23aac6
Nguyen Loc <skipeven2110@gmail.com>
2024-05-08 22:57:53 +0700

    Add project files.

---
+++ NetBarBox/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace NetBarBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bt_thongtinkhachhang = new System.Windows.Forms.Button();
+            this.bt_muado = new System.Windows.Forms.Button();
+            this.bt_goinhanvien = new System.Windows.Forms.Button();
+            this.bt_giohang = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.panel1.Controls.Add(this.bt_goinhanvien);
+            this.panel1.Controls.Add(this.bt_muado);
+            this.panel1.Controls.Add(this.bt_thongtinkhachhang);
+            this.panel1.Location = new System.Drawing.Point(-2, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 645);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.bt_giohang);
+            this.panel2.Location = new System.Drawing.Point(1, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1363, 107);
+            this.panel2.TabIndex = 1;
+            // 
+            // bt_thongtinkhachhang
+            // 
+            this.bt_thongtinkhachhang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.bt_thongtinkhachhang.FlatAppearance.BorderSize = 0;
+            this.bt_thongtinkhachhang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_thongtinkhachhang.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_thongtinkhachhang.ForeColor = System.Drawing.Color.White;
+            this.bt_thongtinkhachhang.Image = ((System.Drawing.Image)(resources.GetObject("bt_thongtinkhachhang.Image")));
+            this.bt_thongtinkhachhang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_thongtinkhachhang.Location = new System.Drawing.Point(0, 110);
+            this.bt_thongtinkhachhang.Margin = new System.Windows.Forms.Padding(0);
+            this.bt_thongtinkhachhang.Name = "bt_thongtinkhachhang";
+            this.bt_thongtinkhachhang.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.bt_thongtinkhachhang.Size = new System.Drawing.Size(245, 79);
+            this.bt_thongtinkhachhang.TabIndex = 2;
+            this.bt_thongtinkhachhang.Text = "       Thông Tin Khách Hàng";
+            this.bt_thongtinkhachhang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_thongtinkhachhang.UseVisualStyleBackColor = false;
+            this.bt_thongtinkhachhang.Click += new System.EventHandler(this.bt_thongtinkhachhang_Click);
+            // 
+            // bt_muado
+            // 
+            this.bt_muado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.bt_muado.FlatAppearance.BorderSize = 0;
+            this.bt_muado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_muado.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_muado.ForeColor = System.Drawing.Color.White;
+            this.bt_muado.Image = ((System.Drawing.Image)(resources.GetObject("bt_muado.Image")));
+            this.bt_muado.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_muado.Location = new System.Drawing.Point(0, 189);
+            this.bt_muado.Margin = new System.Windows.Forms.Padding(0);
+            this.bt_muado.Name = "bt_muado";
+            this.bt_muado.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.bt_muado.Size = new System.Drawing.Size(245, 79);
+            this.bt_muado.TabIndex = 3;
+            this.bt_muado.Text = "       Mua Đồ";
+            this.bt_muado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_muado.UseVisualStyleBackColor = false;
+            this.bt_muado.Click += new System.EventHandler(this.bt_muado_Click);
+            // 
+            // bt_goinhanvien
+            // 
+            this.bt_goinhanvien.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.bt_goinhanvien.FlatAppearance.BorderSize = 0;
+            this.bt_goinhanvien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_goinhanvien.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_goinhanvien.ForeColor = System.Drawing.Color.White;
+            this.bt_goinhanvien.Image = ((System.Drawing.Image)(resources.GetObject("bt_goinhanvien.Image")));
+            this.bt_goinhanvien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_goinhanvien.Location = new System.Drawing.Point(0, 268);
+            this.bt_goinhanvien.Margin = new System.Windows.Forms.Padding(0);
+            this.bt_goinhanvien.Name = "bt_goinhanvien";
+            this.bt_goinhanvien.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.bt_goinhanvien.Size = new System.Drawing.Size(245, 79);
+            this.bt_goinhanvien.TabIndex = 4;
+            this.bt_goinhanvien.Text = "       Gọi Nhân Viên";
+            this.bt_goinhanvien.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_goinhanvien.UseVisualStyleBackColor = false;
+            this.bt_goinhanvien.Click += new System.EventHandler(this.bt_goinhanvien_Click);
+            // 
+            // bt_giohang
+            // 
+            this.bt_giohang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.bt_giohang.FlatAppearance.BorderSize = 0;
+            this.bt_giohang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_giohang.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_giohang.ForeColor = System.Drawing.Color.White;
+            this.bt_giohang.Image = ((System.Drawing.Image)(resources.GetObject("bt_giohang.Image")));
+            this.bt_giohang.Location = new System.Drawing.Point(1257, 0);
+            this.bt_giohang.Margin = new System.Windows.Forms.Padding(0);
+            this.bt_giohang.Name = "bt_giohang";
+            this.bt_giohang.Size = new System.Drawing.Size(106, 107);
+            this.bt_giohang.TabIndex = 4;
+            this.bt_giohang.UseVisualStyleBackColor = false;
+            this.bt_giohang.Click += new System.EventHandler(this.bt_giohang_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Jokerman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(39, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 61);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "LVL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1363, 620);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LVL NetBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bt_thongtinkhachhang;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button bt_muado;
+        private System.Windows.Forms.Button bt_goinhanvien;
+        private System.Windows.Forms.Button bt_giohang;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NetBarBox/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetBarBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_goinhanvien_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_giohang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_muado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_thongtinkhachhang_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
